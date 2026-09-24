8093b78af402b880badff02a23456887206b34a0
medazizghozzi <mohamedaziz.ghozzi@esprit.tn>
2026-09-24 20:21:29 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/medazizghozzi4cce10/Medazizghozzi4cce10Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.medazizghozzi4cce10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medazizghozzi4cce10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Medazizghozzi4cce10Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/medazizghozzi4cce10/Medazizghozzi4cce10ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.medazizghozzi4cce10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medazizghozzi4cce10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
